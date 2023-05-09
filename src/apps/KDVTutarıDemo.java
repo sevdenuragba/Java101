4d1226fee1bf08828db7f4e86e2d371d2a965f25
Sevde Nur <sevdenuragba@gmail.com>
2023-05-09 15:57:58 +0300

    KDVTutarıDemo added

---
+++ src/apps/KDVTutarıDemo.java
@@ -0,0 +1,21 @@
+package apps;
+import java.util.Scanner;
+
+public class KDVTutarıDemo {
+    public static void main(String[] args) {
+    Scanner inp = new Scanner(System.in);
+    System.out.println("Toplam tutarı giriniz.");
+    double tutar = inp.nextDouble();
+    double kdvTutar;
+    if (tutar > 0.0 && tutar < 1000.0) {
+        kdvTutar = tutar * 0.18;
+        System.out.println("KDV tutarı:" + kdvTutar);
+    } else if (tutar > 1000.0) {
+        kdvTutar = tutar * 0.08;
+        System.out.println("KDV tutarı:" + kdvTutar);
+    } else {
+        System.out.println("Lütfen tutarı doğru formatta giriniz");
+    }
+
+}
+}
