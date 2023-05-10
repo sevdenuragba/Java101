9b0b952a19936f361a66a6c104d0068f40df1268
Sevde Nur <sevdenuragba@gmail.com>
2023-05-10 11:58:26 +0300

    UcgenAlanDemo added

---
+++ src/apps/UcgenAlanDemo.java
@@ -0,0 +1,20 @@
+package apps;
+
+import java.util.Scanner;
+
+public class UcgenAlanDemo {
+    public static void main(String[] args) {
+        int a,b,c,u;
+        double alan;
+        Scanner inp=new Scanner(System.in);
+        System.out.println("1. kenarı giriniz.");
+        a= inp.nextInt();
+        System.out.println("2. kenarı giriniz.");
+        b= inp.nextInt();
+        System.out.println("3. kenarı giriniz.");
+        c= inp.nextInt();
+        u=(a+b+c)/2;
+        alan=u*(u-a)*(u-b)*(u-c);
+        System.out.println("Üçgenin Alanı:"+alan);
+    }
+}
