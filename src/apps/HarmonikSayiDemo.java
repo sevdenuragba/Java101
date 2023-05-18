8920633d0dbb963f81ab41ffdfeb86a3dd99b114
Sevde Nur <sevdenuragba@gmail.com>
2023-05-18 15:05:26 +0300

    HarmonikSayiDemo added

---
+++ src/apps/HarmonikSayiDemo.java
@@ -0,0 +1,20 @@
+package apps;
+
+import java.util.Scanner;
+
+public class HarmonikSayiDemo {
+    public static void main(String[] args) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("Bir sayı giriniz: ");
+            int number = scanner.nextInt();
+            double result=0.0;
+
+            for (double i=1;i<=number;i++)
+            {
+                result= result + 1/i;
+            }
+
+            System.out.println("Harmonik seri: "+result);
+
+        }
+}
