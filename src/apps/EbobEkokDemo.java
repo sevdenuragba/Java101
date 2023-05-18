2378ec6bf2b8b60df6e108bd811a41509598fed2
Sevde Nur <sevdenuragba@gmail.com>
2023-05-18 15:33:43 +0300

    EbobEkokDemo added

---
+++ src/apps/EbobEkokDemo.java
@@ -0,0 +1,38 @@
+package apps;
+
+import java.util.Scanner;
+
+public class EbobEkokDemo {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int ebob = 1;
+        System.out.println("Birinci sayıyı giriniz: ");
+        int n1 = scanner.nextInt();
+        System.out.println("İkinci sayıyı giriniz: ");
+        int n2 = scanner.nextInt();
+
+        if (n1 < n2) {
+            int i = n1;
+            while (i >= 1) {
+                if ((n1 % i == 0) && (n2 % i == 0)) {
+                    ebob = i;
+                    System.out.println("Ebob: " + ebob);
+                    break;
+                }
+                i--;
+            }
+        } else {
+            int k = n2;
+            while (k >= 1) {
+                if ((n1 % k == 0) && (n2 % k == 0)) {
+
+                    ebob = k;
+                    System.out.println("Ebob: " + ebob);
+                    break;
+                }
+                k--;
+            }
+        }
+        System.out.println("Ekok: " + ((n1 * n2) / ebob));
+    }
+}
