d2bfccb7d46a03222f04aa6a0a04acfc749213d0
Sevde Nur <sevdenuragba@gmail.com>
2023-05-18 14:42:40 +0300

    BasamakToplamaDemo added

--- src/apps/BasamakToplamaDemo.java
+++ src/apps/BasamakToplamaDemo.java
@@ -5,12 +5,12 @@ import java.util.Scanner;
 public class BasamakToplamaDemo {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int n, basValue, result = 0;
-        System.out.print("Bir sayı giriniz. ");
+        int n, bas, result = 0;
+        System.out.print("Bir sayı giriniz.");
         n = scanner.nextInt();
         while (n != 0) {
-            basValue = n % 10;
-            result += basValue;
+            bas = n % 10;
+            result += bas;
             n /= 10;
         }
         System.out.print("Sayının basamakları toplamı: " + result);
