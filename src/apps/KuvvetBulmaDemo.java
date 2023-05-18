884894f9895600a4de55852b748616117d484911
Sevde Nur <sevdenuragba@gmail.com>
2023-05-18 13:43:11 +0300

    KuvvetBulmaDemo added

---
+++ src/apps/KuvvetBulmaDemo.java
@@ -0,0 +1,20 @@
+package apps;
+
+import java.util.Scanner;
+
+public class KuvvetBulmaDemo {
+    public static void main(String[] args) {
+        int n;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Bir sayı giriniz.");
+        n = scanner.nextInt();
+
+        System.out.println("Dördün kuvvetleri ");
+        for(int i=1; i<n; i*=4)
+            System.out.println(i + "\n");
+
+        System.out.println("Beşin kuvvetleri ");
+        for(int j=1; j<n; j*=5)
+            System.out.println(j + "\n");
+    }
+}
