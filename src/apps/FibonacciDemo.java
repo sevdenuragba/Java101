cb30691c40c1f34468f594a664444007e5afb455
Sevde Nur <sevdenuragba@gmail.com>
2023-05-21 10:38:59 +0300

    FibonacciDemo added

---
+++ src/apps/FibonacciDemo.java
@@ -0,0 +1,23 @@
+package apps;
+
+import java.util.Scanner;
+
+public class FibonacciDemo {
+    public static void main(String[] args) {
+
+        Scanner reader = new Scanner(System.in);
+        int n1 = 0, n2 = 1, n3, i, n;
+        System.out.println("Bir sayı giriniz:");
+        n = reader.nextInt();
+        System.out.print(n1 + " " + n2);
+
+        for(i = 2 ; i < n ; ++i)
+        {
+            n3 = n1 + n2;
+            System.out.print(" " + n3);
+            n1 = n2;
+            n2 = n3;
+        }
+        System.out.println();
+    }
+}
