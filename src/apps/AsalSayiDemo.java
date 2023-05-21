9d966b26bdf469d398e2286ae0b3196400018bd6
Sevde Nur <sevdenuragba@gmail.com>
2023-05-21 10:29:39 +0300

    AsalSayiDemo added

---
+++ src/apps/AsalSayiDemo.java
@@ -0,0 +1,15 @@
+package apps;
+
+public class AsalSayiDemo {
+    public static void main(String[] args) {
+        for (int i = 2; i <= 100; i++) {
+            int n = 0;
+            for (int k = 1; k <= i; k++) {
+                if (i % k == 0)
+                    n++;
+            }
+            if (n<=2)
+                System.out.println(i);
+        }
+    }
+}
