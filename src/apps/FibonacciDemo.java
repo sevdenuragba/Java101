b29b30193fc9ea742efac24ee2ad89577f564a22
Sevde Nur <sevdenuragba@gmail.com>
2023-05-21 10:39:27 +0300

    FibonacciDemo added

--- src/apps/FibonacciDemo.java
+++ src/apps/FibonacciDemo.java
@@ -5,10 +5,10 @@ import java.util.Scanner;
 public class FibonacciDemo {
     public static void main(String[] args) {
 
-        Scanner reader = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
         int n1 = 0, n2 = 1, n3, i, n;
         System.out.println("Bir sayı giriniz:");
-        n = reader.nextInt();
+        n = scanner.nextInt();
         System.out.print(n1 + " " + n2);
 
         for(i = 2 ; i < n ; ++i)
